5c3d6694a56017576c354b217d3306ee47c7f0f7
sajjan3 <sajjan3@gatech.edu>
2024-12-16 22:53:55 -0500

    Initial Commit

---
+++ HideAndSeek/HideAndSeekApp.swift
@@ -0,0 +1,17 @@
+//
+//  HideAndSeekApp.swift
+//  HideAndSeek
+//
+//  Created by Sabina AJJAN on 12/16/24.
+//
+
+import SwiftUI
+
+@main
+struct HideAndSeekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HideAndSeekTests/HideAndSeekTests.swift
@@ -0,0 +1,16 @@
+//
+//  HideAndSeekTests.swift
+//  HideAndSeekTests
+//
+//  Created by Sabina AJJAN on 12/16/24.
+//
+
+import Testing
+
+struct HideAndSeekTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
