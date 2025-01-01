4ed22b2ca95dcbdb4638f4f98b42a895cffb7b55
sajjan3 <sajjan3@gatech.edu>
2024-12-31 22:09:49 -0500

    matchmanager

--- HideAndSeek/MatchManager.swift
+++ HideAndSeek/MatchManager.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import GameKit
 
 class MatchManager: ObservableObject {
     @Published var inGame = false
@@ -16,5 +17,40 @@ class MatchManager: ObservableObject {
     @Published var ourLocation = CGPoint(x: 1, y: 5)
     @Published var winner = ""
     @Published var remainingTime = maxTimeRemaining
+    
+    var match: GKMatch?
+    var otherPlayer: GKPlayer?
+    var localPlayer = GKLocalPlayer.local
+    
+    var playerUUIDKey = UUID().uuidString
+    
+    var rootViewController: UIViewController? {
+        let windowScene  = UIApplication.shared.connectedScenes.first as? UIWindowScene
+        return windowScene?.windows.first?.rootViewController
+    }
+    
+    func authenticateUser() {
+        GKLocalPlayer.local.authenticateHandler = {[self] vc, e in
+            if let viewController = vc {
+                rootViewController?.present(viewController, animated: true)
+                return
+            }
+            if let error = e {
+                authenticationState = .error
+                print(error.localizedDescription)
+                return
+            }
+        }
+        
+        if localPlayer.isAuthenticated {
+            if localPlayer.isMultiplayerGamingRestricted {
+                authenticationState = .restricted
+            } else {
+                authenticationState = .unauthenticated
+            }
+        }
+        
+    }
+    
     
 }
